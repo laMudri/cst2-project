ebea36dd733609f80372e512c8c9d50a84f84281
laMudri <lamudri@gmail.com>
2017-03-05 00:11:31 +0000

    split algorithm into its own module

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -7,66 +7,7 @@ import Data.Array
 import Data.Graph
 
 import Types
-
--- Utils
-
-setAt :: [a] -> Int -> a -> [a]
-setAt [] _ y = []
-setAt (x : xs) 0 y = y : xs
-setAt (x : xs) i y = x : setAt xs (i - 1) y
-
-fixMaybe :: (a -> Maybe a) -> a -> a
-fixMaybe f x =
-  case f x of
-       Nothing -> x
-       Just fx -> fixMaybe f fx
-
-data Phantom a = Ph
-
--- Algorithm
-
-data AlgState k q =
-  AlgState { knownDistances :: [k], addedWeight :: [k], vertexQueue :: q }
-  deriving (Show, Eq)
-
-go :: forall k q. (Eq k, Semiring k, Ord k, Queue q) =>
-      Phantom q -> Graph -> (Edge -> k) -> Vertex -> [k]
-go ph g w source = let AlgState d _ _ = result in d
-  where
-  n :: Int
-  n = length (vertices g)
-
-  initialState :: AlgState k q
-  initialState = AlgState d d s
-    where
-    d :: [k]
-    d = setAt (replicate n zero) source one
-
-    s :: q
-    s = singleton source
-
-  doStep :: AlgState k q -> Maybe (AlgState k q)
-  doStep (AlgState d r s) = do
-    (q , s) <- extract s
-    let r' = r !! q
-    let rN = setAt r q zero
-    let condition e = d !! (snd e) > times r' (w e)
-    let relaxedEdges = filter condition (zip [q, q..] (reachable g q))
-
-    let newWeights a = foldr (\ e a ->
-          setAt a (snd e) (plus (times r' (w e)) (a !! snd e)))
-                             a
-                             relaxedEdges
-
-    let dN = newWeights d
-    let rNN = newWeights rN
-
-    let enqueuedVertices = filter (not . (`elem` s)) (map snd relaxedEdges)
-    let sN = foldr insert s enqueuedVertices
-    return (AlgState dN rNN sN)
-
-  result :: AlgState k q
-  result = fixMaybe doStep initialState
+import Mohri
 
 -- Test data
 
@@ -92,5 +33,5 @@ source = 0
 
 main :: IO ()
 main = do
-  let d = go phantom graph weight source
+  let d = mohri phantom graph weight source
   print d
---
+++ haskell/Mohri.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Mohri where
+
+import Prelude hiding (elem)
+
+import Data.Array
+import Data.Graph
+
+import Types
+
+-- Utils
+
+setAt :: [a] -> Int -> a -> [a]
+setAt [] _ y = []
+setAt (x : xs) 0 y = y : xs
+setAt (x : xs) i y = x : setAt xs (i - 1) y
+
+fixMaybe :: (a -> Maybe a) -> a -> a
+fixMaybe f x =
+  case f x of
+       Nothing -> x
+       Just fx -> fixMaybe f fx
+
+data Phantom a = Ph
+
+-- Algorithm
+
+data AlgState k q =
+  AlgState { knownDistances :: [k], addedWeight :: [k], vertexQueue :: q }
+  deriving (Show, Eq)
+
+mohri :: forall k q. (Eq k, Semiring k, Ord k, Queue q) =>
+      Phantom q -> Graph -> (Edge -> k) -> Vertex -> [k]
+mohri ph g w source = let AlgState d _ _ = result in d
+  where
+  n :: Int
+  n = length (vertices g)
+
+  initialState :: AlgState k q
+  initialState = AlgState d d s
+    where
+    d :: [k]
+    d = setAt (replicate n zero) source one
+
+    s :: q
+    s = singleton source
+
+  doStep :: AlgState k q -> Maybe (AlgState k q)
+  doStep (AlgState d r s) = do
+    (q , s) <- extract s
+    let r' = r !! q
+    let rN = setAt r q zero
+    let condition e = d !! (snd e) > times r' (w e)
+    let relaxedEdges = filter condition (zip [q, q..] (reachable g q))
+
+    let newWeights a = foldr (\ e a ->
+          setAt a (snd e) (plus (times r' (w e)) (a !! snd e)))
+                             a
+                             relaxedEdges
+
+    let dN = newWeights d
+    let rNN = newWeights rN
+
+    let enqueuedVertices = filter (not . (`elem` s)) (map snd relaxedEdges)
+    let sN = foldr insert s enqueuedVertices
+    return (AlgState dN rNN sN)
+
+  result :: AlgState k q
+  result = fixMaybe doStep initialState
