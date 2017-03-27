865e4a7764a353a115dc1b2f8d4987aaf2a7ffc5
laMudri <lamudri@gmail.com>
2017-03-27 14:01:47 +0100

    add priority queue version of k-shortest algorithm

--- haskell/src/Mohri/KShortest.hs
+++ haskell/src/Mohri/KShortest.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DataKinds, KindSignatures, ScopedTypeVariables, GADTs #-}
+{-# LANGUAGE DataKinds, KindSignatures, ScopedTypeVariables,
+             FlexibleContexts #-}
 module Mohri.KShortest where
 
 import Types
 import KSemiring
 import KSemiring.Conversion
 import Mohri
+import MohriPriority
 
 import Data.Graph
 import Data.Proxy
@@ -14,3 +16,9 @@ mohriKShortest ::
   forall k q l. (Eq k, Semiring k, Ord k, Queue q, KnownNat l) =>
   Proxy l -> Proxy q -> Graph -> (Edge -> k) -> Vertex -> [KSemiring l k]
 mohriKShortest _ ph g w source = mohri ph g (convertWeighting w) source
+
+mohripKShortest ::
+  forall k q l.
+    (Eq k, Semiring k, Ord k, PriorityQueue (KSemiring l k) q, KnownNat l) =>
+  Proxy l -> Proxy q -> Graph -> (Edge -> k) -> Vertex -> [KSemiring l k]
+mohripKShortest _ ph g w source = mohrip ph g (convertWeighting w) source
